838e4794f3d8f1b62a19fa8542b22cdbe93184dc
Arturo Velasquez <arturovgr@gmail.com>
2017-04-26 16:18:30 -0500

    some changes

--- shock.py
+++ shock.py
@@ -10,28 +10,27 @@ pression = infile[:,1]
 velocity = infile[:,2]
 
 #Exact solution                                                                                                                                                                                                                              
-density_left = 1.0
-density_right = 0.1
-pressure_left = 1.0
-pressure_right = 0.1
+density_left = 1.0/0.1
+density_right = 1.0
+pressure_left = 1.0/(1.4*0.1)
+pressure_right = 1.0/1.4
 velocity_left = 0.0
 velocity_right = 0.0
-a_right = 1;
 
 def a(pressure, density):
-    return np.sqrt((5.0/3)*(pressure/density))
+    return np.sqrt((1.4)*(pressure/density))
 def compatibility_equation(M):
-    return M-(1.0/M)-a(pressure_left,density_left)*4*(1-((pressure_right/pressure_left)*((5.0/4)*(M**2)-(1.0/4)))**(1.0/5))
+    return M-(1.0/M)-a(pressure_left,density_left)*6*(1-((pressure_right/pressure_left)*((7.0/6)*(M**2)-(1.0/6)))**(1.0/7))
 def Match_number_calculator(f,x0):
     return sciopt.newton_krylov(f,x0)[0]
 
 Ms = Match_number_calculator(compatibility_equation,[2.0])
-pressure_1 = pressure_right*((5.0/4)*(Ms**2)-0.25)
-density_1 = ((1.0/density_right)*((3.0/4)*(1.0/(Ms**2))+(1.0/4)))**(-1)
-velocity_1 = 0.75*(Ms - 1.0/Ms)
+pressure_1 = pressure_right*((7.0/6)*(Ms**2)-(1.0/6))
+density_1 = ((1.0/density_right)*((5.0/6)*(1.0/(Ms**2))+(1.0/6)))**(-1)
+velocity_1 = (5.0/6)*(Ms - 1.0/Ms)
 pressure_2  = pressure_1
 velocity_2  = velocity_1
-density_2  = density_left*((pressure_2/pressure_left)**(3.0/5))
+density_2  = density_left*((pressure_2/pressure_left)**(5.0/7))
 
 def x1(t):
     return 0.5 - a(pressure_left,density_left)*t
@@ -43,13 +42,13 @@ def x4(t):
     return 0.5 + (Ms)*t
 
 def velocity_expansion_fan(x,t):
-    return (3.0/4)*(a(pressure_left, density_left)+((x-0.5)/t))
+    return (5.0/6)*(a(pressure_left, density_left)+((x-0.5)/t))
 def a_expasion_fan(x,t):
-    return a(pressure_left, density_left) - (1.0/3)*velocity_expansion_fan(x,t)
+    return a(pressure_left, density_left) - (0.2)*velocity_expansion_fan(x,t)
 def pressure_expansion_fan(x,t):
-    return pressure_left*((a_expasion_fan(x,t)/a(pressure_left,density_left))**5)
+    return pressure_left*((a_expasion_fan(x,t)/a(pressure_left,density_left))**7)
 def density_expansion_fan(x,t):
-    return ((density_2-density_left)/(x2(t)-x1(t))*x + (density_left*x2(t)-density_2*x1(t))/(x2(t)-x1(t)))
+    return ((a_expasion_fan(x,t)**2)/(1.4*pressure_expansion_fan(x,t)))**(-1)
 
 t = 0.25
 z1 = np.linspace(0,x1(t),25)
@@ -70,11 +69,11 @@ x = np.linspace(0,1,N)
 
 fig = plt.figure()
 ax = plt.axes()
-ax.set_xlabel("x")
-ax.set_ylabel("Pressure")
+ax.set_xlabel("x/L")
+ax.set_ylabel("Pressure (p/gamma*p_r)")
 ax.set_title("P vs x")
-plt.plot(x,pression,'-o',color= "red", label = "Lax-Wendroff ")
-plt.plot(z,y_pressure,label = "Exact solution")
+plt.plot(x,pression,'-o',color= "red", label = "Lax-Wendroff Solution")
+plt.plot(z,y_pressure,label = "Analytical Solution")
 plt.plot(())
 ax.legend()
 filename = 'pressure' 
@@ -83,23 +82,23 @@ plt.close()
 
 fig = plt.figure()
 ax = plt.axes()
-ax.set_xlabel("x")
-ax.set_ylabel("Density")
+ax.set_xlabel("x/L")
+ax.set_ylabel("density (d/d_r)")
 ax.set_title("D vs x")
-plt.plot(x,density,'-o',color= "red",label = "Lax-Wendroff")
-plt.plot(z,y_density,label = "Exact solution")
+plt.plot(x,density,'-o',color= "red",label = "Lax-Wendroff Solution")
+plt.plot(z,y_density,label = "Analytical Solution")
 ax.legend()
-filename = 'Density'
+filename = 'density'
 plt.savefig(filename + '.pdf',format = 'pdf')
 plt.close()
 
 fig = plt.figure()
 ax = plt.axes()
-ax.set_xlabel("x")
-ax.set_ylabel("Velocity")
+ax.set_xlabel("x/L")
+ax.set_ylabel("Velocity (v/a_r)")
 ax.set_title("V vs x")
-plt.plot(x,velocity,'-o',color= "red",label = "Lax-Wendroff")
-plt.plot(z,y_velocity,label = "Exact solution")
+plt.plot(x,velocity,'-o',color= "red",label = "Lax-Wendroff Solution")
+plt.plot(z,y_velocity,label = "Analytical Solution")
 ax.legend()
 filename = 'velocity'
 plt.savefig(filename + '.pdf',format = 'pdf')
